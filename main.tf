57bef30aeba37ea7bcf9202f3d4be5cc43367fe4
SankalpaYogi <139582029+Sankalpayogi@users.noreply.github.com>
2024-08-25 14:48:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
-resource "aws_instance" "EC2_Server" {
+resource "aws_instance" "EC2_Batch_Server" {
   ami             = var.ami
   instance_type   = var.instance_type
   key_name        = var.ec2_key
   security_groups = [aws_security_group.ec2_security_group.name]
 
   tags = {
-    Name = "TF_EC2_Server_1"
+    Name = "TF_EC2_BTH_Server_1"
   }
 }
 
-resource "aws_instance" "EC2_Server" {
+resource "aws_instance" "EC2_App_Server" {
   ami             = var.ami
   instance_type   = var.instance_type
   key_name        = var.ec2_key
   security_groups = [aws_security_group.ec2_security_group.name]
 
   tags = {
-    Name = "TF_EC2_Server_2"
+    Name = "TF_EC2_App_Server_2"
   }
 }
