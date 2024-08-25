44073a54a7deb7c00218cdc0fa0d3e7ca5455581
SankalpaYogi <139582029+Sankalpayogi@users.noreply.github.com>
2024-08-25 15:58:46 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
  backend "s3" {
    bucket = "prafulla-terraform-artifacts"
-   key    = "terraform.tfstate"
+   key    = "prafulla@23"
    region = "ap-south-1"
    encrypt = true
    dynamodb_table = "terraform-state-lock-dynamo"
