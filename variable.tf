0c639dd27b24bfbf35ad9c9fc25d5948679aaa38
SankalpaYogi <139582029+Sankalpayogi@users.noreply.github.com>
2024-09-21 16:23:07 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,30 +1,55 @@
-variable "aws_region" {
-  default = "ap-south-1"
-}
-variable "instance_type" {
-  default = "t2.micro"
-}
-
-variable "ami" {
-  type    = string
-  default = "ami-0522ab6e1ddcc7055"
-}
-
 variable "ec2_key" {
   default = "MaziKey"
 }
 
-variable "ec2_sg" {
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "az" {
+  type    = list(string)
+  default = ["ap-south-1a", "ap-south-1b"]
+}
+
+variable "cidr_blocks" {
   type    = string
-  default = "launch-wizard-1"
+  default = "10.1.0.0/24"
+}
+
+variable "ami" {
+  default = "ami-0522ab6e1ddcc7055"
+}
+
+variable "instance_type" {
+  type    = list(string)
+  default = ["t2.micro", "t2.medium"]
+}
+
+variable "ingress_ports" {
+  type    = list(number)
+  default = [443, 22, 80]
+}
+
+variable "egress_ports" {
+  type    = list(number)
+  default = [443, 22, 80]
+}
+
+variable "protocol_name" {
+  type    = list(string)
+  default = ["tcp", "tcp", "tcp"]
+}
+variable "Instance_name" {
+  type    = list(string)
+  default = ["Web-Instance", "DB-Instance"]
 }
 
 variable "aws_access_key" {
   description = "AWS access key"
-  default     = "AKIAZI2LEVLXCUW2MVOP"
+  default     = "*****************"
 }
 
 variable "aws_secret_key" {
   description = "AWS secret key"
-  default     = "DpM8DG0HmfiiGxhnYPE3hbAwz3fxRgW0XdBtaff2"
+  default     = "***********************"
 }
