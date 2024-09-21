b6d37c45d26f672839d79f9cb34ab20d7816543a
SankalpaYogi <139582029+Sankalpayogi@users.noreply.github.com>
2024-09-21 16:27:01 +0530

    Update security_groups.tf

--- security_groups.tf
+++ security_groups.tf
@@ -1,36 +1,68 @@
-resource "aws_security_group" "ec2_security_group" {
-  name        = "ec2_sg"
-  description = "Allow SSH inbound traffic"
+resource "aws_security_group" "MY-SG" {
+  name = "My-SG"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allows SSH from anywhere
+
+  dynamic "ingress" {
+
+    for_each = var.ingress_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = var.protocol_name[ingress.key]
+      cidr_blocks = [var.cidr_blocks]
+    }
+
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+
+  dynamic "egress" {
+
+    for_each = var.egress_ports
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = var.protocol_name[egress.key]
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
   }
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allows SSH from anywhere
-  }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
-  tags = {
-    Name = "TF_SG"
-  }
-}
+#*************************************** Expanded SG *******************************
+#resource "aws_security_group" "ec2_security_group" {
+#   name        = "ec2_sg"
+#   description = "Allow SSH inbound traffic"
+
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"] # Allows SSH from anywhere
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"] # Allows SSH from anywhere
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   tags = {
+#     Name = "TF_SG"
+#   }
+# }
