5022dc0adbfecb4f719d3c564fd31c7a7f8c0f31
SankalpaYogi <139582029+Sankalpayogi@users.noreply.github.com>
2024-10-16 23:58:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,8 +3,6 @@ provider "aws" {
   # access_key = "AKIA2FLZ3NTJUKXNMICF"
   # secret_key = "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"
 
-
-
   #access_key = var.aws_access_key
   #secret_key = var.aws_secret_key
 }
